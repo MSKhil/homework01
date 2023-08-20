ca1f359d078effaac63db0de366be3212bc62007
Mark Khil <marikya50@gmail.com>
2023-08-20 11:55:52 +1000

    Homework 1 done

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -2,4 +2,19 @@ package ru.otus.homework
 
 
 fun fizzbuzz(n: Int): Array<String> {
+    val a = mutableListOf<String>()
+    for (i in 0..(n - 1)) {
+        if (i == 0) {
+            a.add("FizzBuzz")
+        } else if (((i % 3 == 0) && (i % 5 == 0))) {
+            a.add("FizzBuzz")
+        } else if (i % 5 == 0) {
+            a.add("Buzz")
+        } else if (i % 3 == 0) {
+            a.add("Fizz")
+        } else {
+            a.add("$i")
+        }
+    }
+    return a.toTypedArray()
 }
