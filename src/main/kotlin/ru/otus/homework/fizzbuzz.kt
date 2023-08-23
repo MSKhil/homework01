468e6475f4bf9afcb0d9def0888ebba4b15157cf
Mark Khil <marikya50@gmail.com>
2023-08-24 08:08:12 +1000

    fixup! Homework 1 done

--- src/main/kotlin/ru/otus/homework/fizzbuzz.kt
+++ src/main/kotlin/ru/otus/homework/fizzbuzz.kt
@@ -3,7 +3,7 @@ package ru.otus.homework
 
 fun fizzbuzz(n: Int): Array<String> {
     val a = mutableListOf<String>()
-    for (i in 0..(n - 1)) {
+    for (i in 0 until n) {
         if (i == 0) {
             a.add("FizzBuzz")
         } else if (((i % 3 == 0) && (i % 5 == 0))) {
--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -2,4 +2,14 @@ package ru.otus.homework
 
 
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
+    val two = mutableListOf<Int>()
+    if (numbers[0] + numbers[1] == target) two.add(
+        numbers[0],
+        numbers[1]
+    ) else if (numbers[1] + numbers[2] == target) two.add(
+        numbers[1],
+        numbers[2]
+    ) else if (numbers[2] + numbers[3] == target) two.add(numbers[2], numbers[3])
+    else throw IllegalArgumentException()
+    return two.toIntArray()
 }
